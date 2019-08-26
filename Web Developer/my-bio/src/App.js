e95570cd6ba1651cd09a77f4af6470cc3eb69f12
TadMur <tats.adv.util@gmail.com>
2019-08-25 22:02:16 -0500

    added React.js portfolio within reactJS

--- Web Developer/my-bio/src/App.js
+++ Web Developer/my-bio/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    constructor() {
+        super();
+        this.state = { displayBio: false };
+        this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+    }
+
+    toggleDisplayBio() {
+        this.setState({ displayBio: !this.state.displayBio });
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Tad Mur</h1>
+                <h2>Bio</h2>
+                <p>My name is Tatsuya Tad Murao. I am a freelance web disigner and Japanese language teacher</p>
+                <p>I completed KU Data Analytics Bootcamp.</p>
+                {
+                    this.state.displayBio ? (
+                    <div>
+                            <p>I live in Kansas city, MO. I currently learning React.js.
+                            I graduated KU Data Analytics Bootcamp about a month ago.
+                            I have learned so many computer languages within a short period of time.
+                            Every day I am striving to become a full-stack web developer</p>
+                            <button onClick={this.toggleDisplayBio}>SHOW LESS</button>
+                    </div>) : (
+                        <div><button onClick={this.toggleDisplayBio}>READ MORE</button></div>
+                    )
+                }
+                <div>
+                    <h2>Skills</h2>
+                    <p>React.js, JS, Python(pd, plt, sns, scit-learn), HTML, CSS</p>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ reactJs/portfolio/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React(Title or My name comes here)</h1>
+      <hr />
+    </div>
+  );
+}
+
+export default App;
